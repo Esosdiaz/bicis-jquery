40410f054f4034a5795f7b270e8252623ae0564b
esosdiaz <diazgonzalez.perla@gmail.com>
2017-07-13 03:37:29 -0300

    rescatnado valores de los inputs

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ $(document).ready(function(){
 	$('.submit-btn').click(function(){
 		validateForm();
 	});
+
+	//funcion que se dispara al presionar el boton registrar
+	function validateForm(){
+	//expresiones regulares para establecer la comparacion con el formulario
+		var nombreReg = /^[A-Za-z]+$/;
+		var correoReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+	//Rescatando los valores de los inputs
+		var nombre = $('#name').val();
+        var apellido = $('#lastname').val();
+        var correo = $('#input-email').val();
+        var contrasena = $('#input-password').val();
